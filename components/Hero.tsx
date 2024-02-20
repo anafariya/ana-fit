9f5b0b95ea748c61d05a2c02bfa9f8775e5b4d19
Ana Fariya <anafariya@gmail.com>
2024-02-20 12:30:16 +0530

    made button component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,29 @@
-import Image from "next/image"
-const Hero = () => {
-  return (
-    <div>
-      Main
-    </div>
-  )
-}
+"use client"
 
-export default Hero
+import Image from "next/image";
+import CustomButton from "./CustomButton";
+const Hero = () => {
+    const handleScroll=() => {
+
+    }
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">Forge Your Strength with Me!</h1>
+        <p className="hero__subtitle">
+          Unleash your inner athlete with my comprehensive training platform.
+          From personalized workout plans to expert guidance, I'll help you
+          build strength and crush your fitness goals
+        </p>
+        <CustomButton
+          title = "Explore program"
+          containerStyles="bg-primary-blue 
+          text-white rounded-full mt-10"
+          handleClick = {() => {handleScroll}}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "../../components/Hero";
 
 export default function Home() {
   return (
-    <main className="overflow-hidden text-black">
+    <main className="overflow-hidden text-white">
       <Hero/>
     </main>
   );
